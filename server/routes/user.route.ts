ee35181c07597ad25d2e25ebb8aef6017d52ea55
Eduardo Mendes <eduardo.mendes@sensedia.com>
2023-12-10 10:48:03 -0300

    add and get members in admin dashboard

---
+++ server/routes/user.route.ts
@@ -0,0 +1,45 @@
+import express from "express";
+import {
+  activateUser,
+  getAllUsers,
+  getUserInfo,
+  loginUser,
+  logoutUser,
+  registrationUser,
+  socialAuth,
+  updateAccessToken,
+  updatePassword,
+  updateProfilePicture,
+  updateUserInfo,
+  updateUserRole,
+} from "../controllers/user.controller";
+import { authorizeRoles, isAuthenticated } from "../middleware/auth";
+const userRouter = express.Router();
+
+//Post
+userRouter.post("/registration", registrationUser);
+userRouter.post("/activate-user", activateUser);
+userRouter.post("/login", loginUser);
+userRouter.post("/social-auth", socialAuth);
+//Get
+userRouter.get("/refresh", updateAccessToken);
+userRouter.get("/logout", isAuthenticated, logoutUser);
+userRouter.get("/me", isAuthenticated, getUserInfo);
+userRouter.get(
+  "/get-users",
+  isAuthenticated,
+  authorizeRoles("admin"),
+  getAllUsers
+);
+//Put
+userRouter.put("/update-user-info", isAuthenticated, updateUserInfo);
+userRouter.put("/update-user-password", isAuthenticated, updatePassword);
+userRouter.put("/update-user-avatar", isAuthenticated, updateProfilePicture);
+userRouter.put(
+  "/update-user",
+  isAuthenticated,
+  authorizeRoles("admin"),
+  updateUserRole
+);
+
+export default userRouter;
